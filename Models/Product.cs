e440edee0131ea66274d8fd9298e808b83741940
MooPooNGit <moopoon.noi@gmail.com>
2018-04-08 14:05:55 +0700

    first commit

---
+++ Models/Product.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace WebServer.Models
+{
+    public class Product
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+    }
+}
